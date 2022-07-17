5d4e22470599c56cd0de588d2ca6732f8aa7d4a2
Sergey Tikka <sergofan88@yandex.ru>
2022-07-17 09:43:47 +0300

    fixed Task 2

--- task_2/solution.js
+++ task_2/solution.js
@@ -18,13 +18,13 @@ function calcShipping(sum, min, shipping) {
 
     let shippingSum;
     
-    if (productsSum == 0) {
-        shippingSum = 0;
-    } else if (productsSum >= freeShippingMinSum){
+    if (productsSum == 0 || productsSum >= freeShippingMinSum ) {
         shippingSum = 0;
     } else if (productsSum > 0 && productsSum < freeShippingMinSum){
-      shippingSum = shippingPrice;
-    } else {shippingSum = shippingPrice};
+          shippingSum = shippingPrice;
+    } else {
+        shippingSum = shippingPrice;
+    }
     
 
     // Конец решения задания №2.1.
@@ -45,11 +45,7 @@ function calcDiscount(sum, min, discount) {
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
 
-    let discountSum;
-
-    if (productsSum >= discountMinSum){
-        discountSum = discountPart * productsSum / 100
-    } else {discountSum = 0};
+    let discountSum = productsSum >= discountMinSum ? discountPart * productsSum / 100 : 0;
 
     // Конец решения задания №2.2.
 
